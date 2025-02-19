b9ca366942af797e0bf74cdd700d42d141e90198
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2025-02-19 07:29:43 +0100

    read cli args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let _task = std::env::args().nth(1).expect("No task given!");
+    let _name = std::env::args().nth(2).expect("No task name given!");
+
+    println!("Task {_task} '{_name}'")
+
 }
