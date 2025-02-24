1c48cc097f63e3871cb20a500426d6869506565c
Sebastian Wdowiarz <wdowiarzsebastian@gmail.com>
2025-02-24 07:46:08 +0100

    Use clap to get and pare cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,25 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    action: Action,
+}
+
+#[derive(Subcommand)]
+enum Action {
+    Add {data:String},
+    Update{id:i32, updated_data: String},
+    Delete{id:i32},
+    List,
+}
+
 fn main() {
-    let _task = std::env::args().nth(1).expect("No task given!");
-    let _data = std::env::args().nth(3).expect("No task name given!");
-    let _task_id = std::env::args().nth(2).expect("No task ID given!");
-
-    match _task.as_str() {
-        "add" => println!("{_task} function"),
-        "update" => {
-            println!("{_task} function");
-            let task_id = _task_id.trim().parse::<i32>().expect("Unable to parse task ID!");
-            println!("updating ID:{task_id} with data: {_data}")
-        }
-        "list" => println!("{_task} all tasks!"),
-        "delete" => {
-            println!("{_task} function");
-            let task_id = _task_id.trim().parse::<i32>().expect("Unable to parse task ID!");
-            println!("Deleting task ID: {task_id}")
-        }
-        "mark" => {
-            println!("{_task} function");
-            let task_id = _task_id.trim().parse::<i32>().expect("Unable to parse task ID!");
-            println!("mark task ID: {task_id} as {_data}")
-        }
-        _ => {
-            println!("Wrong action: {_task}!")
-        }
+    let cli = Cli::parse();
+    match cli.action {
+        Action::Add { data } => println!("Adding task: {data}"),
+        Action::Update { id, updated_data } => println!("Updating data: {id} with {updated_data}"),
+        Action::Delete { id } => println!("Deleting data: {id}"),
+        Action::List => println!("Listing all data!"),
     }
-
 }
